96f6caeee351fce521df78fd38c08206646b1768
badlibrarian <badlibrarianstudio@gmail.com>
2024-08-25 10:53:35 +0100

    Add files via upload
    build

---
+++ precache-manifest.74fe33351da36aae18f15d57d680018a.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e333c2b19ee431a2b90d0b595ea4e54",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9bb5431645143f4d3363",
+    "url": "/static/css/main.03083f3d.chunk.css"
+  },
+  {
+    "revision": "a471024d66d6ccac5761",
+    "url": "/static/js/2.f64cc395.chunk.js"
+  },
+  {
+    "revision": "ac090904937ff15ba3f7",
+    "url": "/static/js/3.18a8d937.chunk.js"
+  },
+  {
+    "revision": "9bb5431645143f4d3363",
+    "url": "/static/js/main.0ef8157f.chunk.js"
+  },
+  {
+    "revision": "7630ab4973f3feffff9e",
+    "url": "/static/js/runtime~main.91c6a37f.js"
+  },
+  {
+    "revision": "3fc5383ee0b7e708a6ba43d610541eb0",
+    "url": "/static/media/PPMondwest-Regular.3fc5383e.otf"
+  },
+  {
+    "revision": "f67a0ca88b3c94ce1ee9c3ab0fac8385",
+    "url": "/static/media/PPNeueBit-Bold.f67a0ca8.otf"
+  },
+  {
+    "revision": "034de38c65e202c1cc838e7d014385fd",
+    "url": "/static/media/VT323-Regular.034de38c.ttf"
+  },
+  {
+    "revision": "f38cce97ee398185599be2264a0c49da",
+    "url": "/static/media/anokhina.f38cce97.png"
+  },
+  {
+    "revision": "a9d48c5486382be44b3c8f983d3bf5f6",
+    "url": "/static/media/banner.a9d48c54.jpeg"
+  },
+  {
+    "revision": "742fb7b573f935c14ac71f591ca4569b",
+    "url": "/static/media/cahill.742fb7b5.png"
+  },
+  {
+    "revision": "13c982bf115847af4bb6ae61de683b23",
+    "url": "/static/media/charactercarousel.13c982bf.png"
+  },
+  {
+    "revision": "1df0533b9358154109e4a98e202b4790",
+    "url": "/static/media/clemmie.1df0533b.png"
+  },
+  {
+    "revision": "68adfe7e2b0cd7f4320587eb99ebd264",
+    "url": "/static/media/compPlant.68adfe7e.png"
+  },
+  {
+    "revision": "690987639c46b799876006cb99153a96",
+    "url": "/static/media/computer.69098763.png"
+  },
+  {
+    "revision": "3a17c4f9a3fb27ca8e5dad4b948a506d",
+    "url": "/static/media/emily.3a17c4f9.png"
+  },
+  {
+    "revision": "30fa52f905c48b12a16967d47f631a63",
+    "url": "/static/media/flower.30fa52f9.png"
+  },
+  {
+    "revision": "9d4bd07633b41b59c3ad4bd485bda3eb",
+    "url": "/static/media/goda.9d4bd076.png"
+  },
+  {
+    "revision": "500675682f3192e0113255c4bdd93f8a",
+    "url": "/static/media/junkdrawer.50067568.jpeg"
+  },
+  {
+    "revision": "ebaff0b08e474a7e7dd40acca2253e18",
+    "url": "/static/media/lib.ebaff0b0.png"
+  },
+  {
+    "revision": "d0a66c62a6b0e835f11fc60d5bef4eed",
+    "url": "/static/media/meis.d0a66c62.jpg"
+  },
+  {
+    "revision": "7dd112ab7de2562e5e34f676bd8b67fb",
+    "url": "/static/media/pan.7dd112ab.png"
+  },
+  {
+    "revision": "907ea4bcb4466a8fa7ad1466980b619d",
+    "url": "/static/media/posters.907ea4bc.png"
+  },
+  {
+    "revision": "cded2b77e82e4044c92a8baecb383055",
+    "url": "/static/media/sofia.cded2b77.png"
+  },
+  {
+    "revision": "f9782e50d18fb26a358a3d16082169bb",
+    "url": "/static/media/startup.f9782e50.png"
+  },
+  {
+    "revision": "73b1a13a3c059be9448361885a31981a",
+    "url": "/static/media/vandal.73b1a13a.png"
+  },
+  {
+    "revision": "2f5f06b5fba6f7a64e181ae6f2a1502a",
+    "url": "/static/media/vertel.2f5f06b5.jpg"
+  },
+  {
+    "revision": "33a256cb50e647be062197bfff0ad1c4",
+    "url": "/static/media/video.33a256cb.mov"
+  }
+]);
